7b4d8b9329c63dd3a0fe194fd8f3d8d373da8b5c
Ariel <fullmetalbrackets@gmail.com>
2023-11-17 04:06:52 -0500

    remove collections build cache, breaks build

--- astro.config.ts
+++ astro.config.ts
@@ -4,8 +4,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://techservatory.com',
-  experimental: {
-    contentCollectionCache: true,
-  },
   integrations: [sitemap()]
 });
